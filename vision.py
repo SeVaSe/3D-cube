90e4cf18bc7be3d59a264b14c28031a18f4e315d
sevase <kotaseva8@gmail.com>
2023-08-14 21:43:20 +0300

    TIK-001: окно для отрисовки

--- vision.py
+++ vision.py
@@ -1,29 +1,31 @@
+import cv2
+import OpenGL
+import numpy as np
+import pygame as pg
 
 
+class SoftRender:
+    def __init__(self):
+        pg.init()
+        self.RES = self.WIDTH, self.HEIGHT = 1200, 700
+        self.H_WIDTH, H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
+        self.FPS = 60
+        self.screen = pg.display.set_mode(self.RES)
+        self.clock = pg.time.Clock()
+
+    def draw(self):
+        self.screen.fill(pg.Color('black'))
+
+    def run(self):
+        while True:
+            self.draw()
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            pg.display.set_caption(str(self.clock.get_fps()))
+            pg.display.flip()
+            self.clock.tick(self.FPS)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app = SoftRender()
+    app.run()
 
